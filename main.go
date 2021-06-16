dfe968881979704e700049cf71095057ab16287f
golamrabbiazad <golamrabbiazad@gmail.com>
2021-06-16 23:05:22 +0600

    colly v2 + export=csv

--- main.go
+++ main.go
@@ -1,35 +1,40 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
 	"strconv"
 	"strings"
 
-	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/v2"
 )
 
 // DrugsList Types specified
-type DrugsList struct {
-	Title       string
-	Power 		string
-	URL         string
-	Price		float64
-}
+// type DrugsList struct {
+// 	Title string
+// 	Power string
+// 	URL   string
+// 	Price float64
+// }
 
 func main() {
-	drug := []DrugsList{}
+	// drug := []DrugsList{}
+	fName := "drug_list.csv"
 
-	// fName := "drug_list.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+		return
+	}
 
-	// file, err := os.Create(fName); if err != nil {
-	// 	log.Fatalf("Cannot create file %q: %s\n", fName, err)
-	// 	return
-	// }
+	defer file.Close()
 
-	// defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
 
-	// writer := csv.NewWriter(file)
-	// defer writer.Flush()
+	writer.Write([]string{"Title", "Power", "Price", "URL"})
 
 	c := colly.NewCollector(colly.AllowedDomains("medex.com.bd"))
 
@@ -37,20 +42,20 @@ func main() {
 		getTitle := e.Text + "\n"
 		title := strings.TrimSpace(getTitle)
 
-		fmt.Println(title)
+		writer.Write([]string{title})
 	})
 
 	c.OnHTML("span.grey-ligten", func(h *colly.HTMLElement) {
 		quantity := strings.TrimSpace(h.Text)
-		fmt.Println(quantity)
+		writer.Write([]string{quantity})
 	})
 
 	c.OnHTML("a.hoverable-block", func(l *colly.HTMLElement) {
 		links := l.Attr("href")
 
-		temp := DrugsList{}
-		temp.URL = links
-		drug = append(drug, temp)
+		// temp := []DrugsList{}
+		// temp.URL = links
+		// drug = append(drug, temp)
 
 		c.OnHTML("span.pack-size-info", func(p *colly.HTMLElement) {
 			str := p.Text + "\n"
@@ -58,23 +63,39 @@ func main() {
 
 			prev := strings.Trim(priceStr, "(")
 			aft := strings.Trim(prev, ")")
-		
+
 			modStr := strings.Split(aft, " ")
 			price := modStr[len(modStr)-1]
-		
-			if val, err := strconv.ParseFloat(price, 64); err == nil {
-				fmt.Println(val)
-			}
+
+			writer.Write([]string{price})
+
+			// if val, err := strconv.ParseFloat(price, 64); err == nil {
+			// 	fmt.Println(val)
+			// }
 		})
 
 		c.Visit(links)
 	})
 
-	pages := 28
+	var pages int
+	var companyURL string
 
-	for i := 1; i <= pages; i++ {
-		c.Visit("https://medex.com.bd/companies/73/square-pharmaceuticals-ltd?page=" + strconv.Itoa((i)))
+	fmt.Printf("How many page do you want to scrape?")
+	fmt.Scanln(&pages)
+
+	companyURL = "https://medex.com.bd/companies/73/square-pharmaceuticals-ltd"
+
+	visitURL(1, pages, companyURL, c)
+
+	log.Printf("Scraping finished, check file %q for results\n", fName)
+
+	log.Println(c)
+
+	// fmt.Println(drug)
+}
+
+func visitURL(from, to int, companyURL string, c *colly.Collector) {
+	for i := from; i <= to; i++ {
+		c.Visit(companyURL + "?page=" + strconv.Itoa(i))
 	}
-
-	fmt.Println(drug)
 }
